2a564c8714dc0b06810fccc10b493aa172471d35
Krzysztof Saczuk <zakku@zakku.eu>
2023-02-27 22:10:36 +0100

    fix(neovim): deprecated sumneko_lua + colorizer

--- .config/nvim/lua/custom/plugins/lspconfig.lua
+++ .config/nvim/lua/custom/plugins/lspconfig.lua
@@ -3,35 +3,35 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 local servers = {
-  "astro",
-  "cssls",
-  "cssmodules_ls",
-  "dockerls",
-  "graphql",
-  "html",
-  "jdtls",
-  "lua_ls",
-  "jsonls",
-  "prismals",
-  "eslint",
-  "tsserver",
-  "marksman",
-  "pyright",
-  "rust_analyzer",
-  "sqlls",
-  "svelte",
-  "taplo",
-  "volar",
-  "yamlls",
-  "gopls"
+	"astro",
+	"cssls",
+	"cssmodules_ls",
+	"dockerls",
+	"graphql",
+	"html",
+	"jdtls",
+	-- "lua_ls", // Disable overriding lua_ls config
+	"jsonls",
+	"prismals",
+	"eslint",
+	"tsserver",
+	"marksman",
+	"pyright",
+	"rust_analyzer",
+	"sqlls",
+	"svelte",
+	"taplo",
+	"volar",
+	"yamlls",
+	"gopls",
 }
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    -- root_dir = function()
-    --   return vim.loop.cwd()
-    -- end,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		-- root_dir = function()
+		--   return vim.loop.cwd()
+		-- end,
+	})
 end
