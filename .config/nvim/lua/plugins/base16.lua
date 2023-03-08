44329a5cccb348451755f73b49b674af0eadfec1
Krzysztof Saczuk <zakku@zakku.eu>
2023-03-08 16:53:44 +0100

    feat(neovim): add lsp to lualine

--- .config/nvim/lua/plugins/base16.lua
+++ .config/nvim/lua/plugins/base16.lua
@@ -2,7 +2,7 @@ return {
   {
     "RRethy/nvim-base16",
     config = function(colors)
-      require("base16-colorscheme").setup({
+      local colors = {
         base00 = '#242424', -- '#282a36'
         base01 = '#34353e',
         base02 = '#43454f',
@@ -19,9 +19,31 @@ return {
         base0D = '#57c7ff',
         base0E = '#ff6ac1',
         base0F = '#b2643c',
-      }, {
-        notify = false,
-      })
+      }
+
+      require("base16-colorscheme").setup(colors, { notify = false })
+
+      -- Lualine highlights
+
+      -- LSP status component
+      vim.api.nvim_set_hl(0, "lualine_lsp_status", { fg = colors.base0D, bg = colors.base01 })
+
+      -- LSP progress component
+      vim.api.nvim_set_hl(0, "lualine_lsp_progress", { fg = colors.base0B, bg = colors.base01 })
+
+      -- LSP diagnostic component
+      vim.api.nvim_set_hl(0, "lualine_lsp_diagnostic_error", { fg = colors.base08, bg = colors.base01 })
+      vim.api.nvim_set_hl(0, "lualine_lsp_diagnostic_warn", { fg = colors.base0A, bg = colors.base01 })
+      vim.api.nvim_set_hl(0, "lualine_lsp_diagnostic_info", { fg = colors.base0B, bg = colors.base01 })
+      vim.api.nvim_set_hl(0, "lualine_lsp_diagnostic_hint", { fg = colors.base0E, bg = colors.base01 })
+
+      -- CWD component
+      vim.api.nvim_set_hl(0, "lualine_cwd_icon", { fg = colors.base00, bg = colors.base08 })
+      vim.api.nvim_set_hl(0, "lualine_cwd_text", { fg = colors.base07, bg = colors.base02 })
+
+      -- Progress component
+      vim.api.nvim_set_hl(0, "lualine_progress_icon", { fg = colors.base00, bg = colors.base0B })
+      vim.api.nvim_set_hl(0, "lualine_progress_text", { fg = colors.base0B, bg = colors.base02 })
     end
   }
 }
