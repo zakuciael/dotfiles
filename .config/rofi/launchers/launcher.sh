76c82c7ceadef27b21c289e2a24dde8038e4ee8c
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-09-23 14:05:08 +0200

    feat: switch ulauncher to rofi

---
+++ .config/rofi/launchers/launcher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## Author : Aditya Shakya (adi1090x)
+## Github : @adi1090x
+#
+## Rofi   : Launcher (Modi Drun, Run, File Browser, Window)
+
+## Run
+rofi \
+    -theme "$HOME/.config/rofi/launchers/style.rasi" \
+    -show $1
