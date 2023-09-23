4c346962cf1ec8daf9e93e49d4a91773fee78dac
Krzysztof Saczuk <me@krzysztofsaczuk.pl>
2023-09-23 14:26:16 +0200

    refactor(scripts): add support for bspwm in screenshot script + keybinds

--- .scripts/screenshot.sh
+++ .scripts/screenshot.sh
@@ -3,14 +3,24 @@
 SAVE_TO_FILE=false
 
 get_focused_monitor() {
-  hyprctl monitors -j | jq -r '.[] | select(.focused) | .name'
+  if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+    xrandr | rg "$(bspc query -M --names -m .focused) .+ ([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)" -or '${1}x${2}+${3}+${4}'
+  else
+    hyprctl monitors -j | jq -r '.[] | select(.focused) | .name'
+  fi
 }
 
 get_active_window() {
-  hyprctl activewindow -j | jq -r '"\(.at[0]-5),\(.at[1]-5) \(.size[0]+10)x\(.size[1]+10)"'
+  if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+    bspc query -T -n $(bspc query -N -n .focused) | jq -r '.client | { border: .borderWidth, rect: .[(.state + "Rectangle")]} | "\(.rect.width+(.border*2))x\(.rect.height+(.border*2))+\(.rect.x)+\(.rect.y)"'
+  else
+    hyprctl activewindow -j | jq -r '"\(.at[0]-5),\(.at[1]-5) \(.size[0]+10)x\(.size[1]+10)"'
+  fi
+
 }
 
 get_file_name() {
+  # echo "/tmp/test.png"
   echo "$(xdg-user-dir PICTURES)/Screenshots/$(date +'%Y-%m-%d_%H-%M.png')"
 }
 
@@ -60,24 +70,48 @@ done
 
 case "$1" in
   window)
-    if [[ $SAVE_TO_FILE == true ]]; then
-      grim -g "$(get_active_window)" "$(get_file_name)"
+    if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+      if [[ $SAVE_TO_FILE == true ]]; then
+        maim -g "$(get_active_window)" "$(get_file_name)"
+      else
+        maim -g "$(get_active_window)" | xclip -selection clipboard -t image/png
+      fi
     else
-      grim -g "$(get_active_window)" - | wl-copy
+      if [[ $SAVE_TO_FILE == true ]]; then
+        grim -g "$(get_active_window)" "$(get_file_name)"
+      else
+        grim -g "$(get_active_window)" - | wl-copy
+      fi
     fi
     ;;
   screen)
-    if [[ $SAVE_TO_FILE == true ]]; then
-      grim -o "$(get_focused_monitor)" "$(get_file_name)"
+    if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+      if [[ $SAVE_TO_FILE == true ]]; then
+        maim -g "$(get_focused_monitor)" "$(get_file_name)"
+      else
+        maim -g "$(get_focused_monitor)" | xclip -selection clipboard -t image/png
+      fi
     else
-      grim -o "$(get_focused_monitor)" - | wl-copy
+      if [[ $SAVE_TO_FILE == true ]]; then
+        grim -o "$(get_focused_monitor)" "$(get_file_name)"
+      else
+        grim -o "$(get_focused_monitor)" - | wl-copy
+      fi
     fi
     ;;
   region)
-    if [[ $SAVE_TO_FILE == true ]]; then
-      grim -g "$(slurp)" "$(get_file_name)"
+    if [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+      if [[ $SAVE_TO_FILE == true ]]; then
+        maim -s "$(get_file_name)"
+      else
+        maim -s | xclip -selection clipboard -t image/png
+      fi
     else
-      grim -g "$(slurp)" - | wl-copy
+      if [[ $SAVE_TO_FILE == true ]]; then
+        grim -g "$(slurp)" "$(get_file_name)"
+      else
+        grim -g "$(slurp)" - | wl-copy
+      fi
     fi
     ;;
   *)
